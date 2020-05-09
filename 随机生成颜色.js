a107d5f018a1ce71c21db3ac2d57f8a4df76695b
limj <32104999+bmkk@users.noreply.github.com>
2020-05-09 10:34:09 +0800

    Update 随机生成颜色.js

--- 随机生成颜色.js
+++ 随机生成颜色.js
@@ -2,5 +2,5 @@ function rc(){
         var r=Math.floor(Math.random()*256);
         var g=Math.floor(Math.random()*256);
         var b=Math.floor(Math.random()*256);
-        returen `rgb(${r},${g},${b})`;
+        return `rgb(${r},${g},${b})`;
     }
